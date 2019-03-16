77fbe6960968bbf7ba714ccfffe7d60ca22fb75f
Matthew Hargraves <matthew.hargraves@cef.co.uk>
2019-03-16 18:43:59 +0000

    key

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function preload() {
   mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
     clon + ',' + clat + ',' + zoom + '/' +
     ww + 'x' + hh +
-    `?access_token=pk.${MAP_API}`);
+    `?access_token=${MAP_API}`);
 
     // earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv");
     // earthquakes = loadStrings("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.csv");
